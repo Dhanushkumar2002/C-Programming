dfc4d39878f24d2e97292a70f4ed5d196db89cff
venkata dhanush kumar chembeti <dhanushkumar7000@gmail.com>
2025-03-12 02:16:20 +0530

    deploy

---
+++ Variables.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int number = 25;
+    char star = '*';
+    int age = 23;
+    float Pi = 3.14;
+
+// variables are case senstive, Both are different variables.
+    int A = 1;
+    int a = 1;
+
+// 1st character is Alphabet or '_'.
+    int _one = 10;
+    int One = 20;
+
+// No comma / blank space
+    // int final price = 100;
+
+// No other Symbols other than '_'
+    int final_prize = 100;
+}
